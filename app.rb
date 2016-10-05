4d6edae8ef628907d11786fd83f1c37b369669bd
Domonique Griffin <griffind2009@gmail.com>
2016-10-05 11:56:49 -0400

    MVP set-up

---
+++ app.rb
@@ -0,0 +1,12 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'active_record'
+
+require_relative 'db/connection'
+
+require_relative 'models/pokemon'
+
+get '/pokemons' do
+  @pokemons = Pokemon.all
+  erb :index
+end
